71a7b46cf9f84588a38cb0197a4543c47af3044f
kunisettysarath <kunisettysarath@gmail.com>
2022-04-20 19:12:16 -0500

    icp 11 source and documentation

---
+++ MobileDevelopment/ICP's/ICP11/Source/app/src/main/java/com/example/icp11/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.icp11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText textEdit;
+    Button btn;
+    TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textEdit = findViewById(R.id.editTextTTS);
+        btn = findViewById(R.id.buttonTTS);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tts = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if (status == TextToSpeech.SUCCESS) {
+                            int result = tts.setLanguage(Locale.US);
+                            if (result == TextToSpeech.LANG_NOT_SUPPORTED ||
+                                    result == TextToSpeech.LANG_MISSING_DATA)
+                                Log.e("message", "language is not supported");
+                            else {
+                                // calling the speak method
+                                speak();
+                            }
+                        } else
+                            Log.e("message", "TTS is not supported");
+                    }
+                });
+            }
+                               }
+        );
+
+    }
+
+    void speak() {
+        String s = String.valueOf(textEdit.getText());
+        if(s.length()==0) {
+            tts.speak("please enter something", TextToSpeech.QUEUE_ADD, null);
+        }
+        else {
+            Log.i("message", s);
+            Log.i("message", "about to speak");
+            tts.speak(s,
+                    TextToSpeech.QUEUE_ADD, null);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // whenever the app is in background the speach stops
+        tts.stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //shuting down the speech service so that other applications will be able to use this
+        tts.shutdown();
+    }
+}
